8a248d036309788be6f569caea73419c6709053c
anaumchev <anaumchev@gmail.com>
2022-01-25 20:21:02 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -l
 export ISE_EIFFEL=/usr/local/Eiffel
 export ISE_PLATFORM=linux-x86-64
-compilation=`$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin/autoproof.sh -batch -config *.ecf`
-verification=`$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin/autoproof.sh -batch -config *.ecf -verify collection:cluster`
-#time=$(date)
+$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin/autoproof.sh -batch -config *.ecf
+compilation=`cat autoproof_output.txt`
+$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin/autoproof.sh -batch -config *.ecf -verify collection:cluster
+verification=`cat autoproof_output.txt`
 echo "::set-output name=compilation::$compilation"
 echo "::set-output name=verification::$verification"
