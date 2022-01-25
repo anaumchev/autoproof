e62ad16894ebc64f2fbad3b4574d7eedd0cf4d1c
anaumchev <anaumchev@gmail.com>
2022-01-25 20:45:16 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 export ISE_EIFFEL=/usr/local/Eiffel
 export ISE_PLATFORM=linux-x86-64
-$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin/autoproof.sh -batch -config *.ecf
+compilation_result=`$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin/autoproof.sh -batch -config *.ecf`
 compilation=`cat autoproof_output.txt`
 compilation=${compilation//'%'/'%25'}
 compilation=${compilation//$'\n'/'%0A'}
 compilation=${compilation//$'\r'/'%0D'}
-$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin/autoproof.sh -batch -config *.ecf -verify collection:cluster
+echo "::set-output name=compilation::$compilation"
+if [[ $compilation_result != 0 ]]; then
+  exit $compilation_result;
+fi
+verification_result=`$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin/autoproof.sh -batch -config *.ecf -verify collection:cluster`
 verification=`cat autoproof_output.txt`
 verification=${verification//'%'/'%25'}
 verification=${verification//$'\n'/'%0A'}
 verification=${verification//$'\r'/'%0D'}
-echo "::set-output name=compilation::$compilation"
 echo "::set-output name=verification::$verification"
+exit $verification_result
